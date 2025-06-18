4cc5e5347003cedf54392bb468eea6b0f4df66b5
anhteo94 <phamvannghia20066@gmail.com>
2025-06-19 02:06:02 +0700

    Update PhamNghiaHub.lua

--- PhamNghiaHub.lua
+++ PhamNghiaHub.lua
@@ -1,108 +1,50 @@
--- Phạm Nghĩa Hub (v1.0)
--- Yêu cầu key: phamnghia123
+-- Phạm Nghĩa Hub (v1.0) – Không cần key, có thể kéo được
 
--- Phạm Nghĩa Hub (v1.0)
-
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/anhteo94/HanhTrinhMoi/main/Library.lua"))()
+-- Load UI library
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/library.lua"))()
 local Window = Library.CreateLib("Phạm Nghĩa Hub", "Ocean")
-Window.draggable = true  -- ← Cho phép kéo
 
-Window:Init()  -- Gọi hàm tạo UI chính
+-- Cho phép kéo menu
+Window.draggable = true
 
-    
-end)
+-- Khởi tạo UI chính
+Window:Init(function()
+    -- ==== TAB: Thông Tin =====
+    local t1 = Window:NewTab("📌 Thông Tin")
+    local s1 = t1:NewSection("User Info")
+    s1:NewLabel("Tên: "..game.Players.LocalPlayer.Name)
+    s1:NewLabel("Level: <chưa tích hợp>")
+    s1:NewLabel("Map hiện tại: <chưa tích hợp>")
+    s1:NewLabel("Trạng thái: Đang chờ")
 
--- Hàm hỗ trợ tạo UI chỉ chạy sau xác thực
-function Window:Init()
-    -- Thông Tin chung
-    do
-        local t = Window:NewTab("📌 Thông Tin")
-        local s = t:NewSection("User info")
-        s:NewLabel("Tên: "..game.Players.LocalPlayer.Name)
-        s:NewLabel("Level: <chưa tích hợp>")
-        s:NewLabel("Map hiện tại: <chưa tích hợp>")
-        s:NewLabel("Trạng thái: Đang chờ")
-    end
+    -- ==== TAB: Auto Farm =====
+    local t2 = Window:NewTab("⚔️ Auto Farm")
+    local s2 = t2:NewSection("Cài đặt farm")
+    s2:NewToggle("Auto Farm Level", "", function(v) _G.AutoFarmLevel = v end)
+    s2:NewToggle("Auto Farm Bone", "", function(v) _G.AutoFarmBone = v end)
+    s2:NewToggle("Auto Farm Chest", "", function(v) _G.AutoFarmChest = v end)
+    s2:NewToggle("Auto Farm Mastery", "", function(v) _G.AutoFarmMaster = v end)
 
-    -- Auto Farm
-    do
-        local t = Window:NewTab("⚔️ Auto Farm")
-        local s = t:NewSection("Cài đặt farm")
-        s:NewToggle("Auto Farm Level", "", function(v) _G.AutoFarmLevel = v end)
-        s:NewToggle("Auto Farm Bone", "", function(v) _G.AutoFarmBone = v end)
-        s:NewToggle("Auto Farm Chest", "", function(v) _G.AutoFarmChest = v end)
-        s:NewToggle("Auto Farm Mastery", "", function(v) _G.AutoFarmMaster = v end)
-    end
+    -- ==== TAB: Auto Boss =====
+    local t3 = Window:NewTab("👑 Auto Boss")
+    local s3 = t3:NewSection("Boss")
+    s3:NewButton("Auto Kill Saber", "", function()
+        -- TODO: code kill boss ở đây
+    end)
+    s3:NewButton("TP đến Boss", "", function()
+        -- TODO: teleport đến boss
+    end)
 
-    -- Auto Boss
-    do
-        local t = Window:NewTab("👑 Auto Boss")
-        local s = t:NewSection("Boss")
-        s:NewButton("Auto Kill Saber", "", function() end)
-        s:NewButton("Auto Kill Buddha", "", function() end)
-        s:NewButton("Teleport đến boss", "", function() end)
-        s:NewButton("Auto lấy Haki từ Boss", "", function() end)
-    end
+    -- ==== TAB: Misc =====
+    local t4 = Window:NewTab("⚙️ Misc")
+    local s4 = t4:NewSection("Tùy chỉnh chung")
+    s4:NewSlider("Tốc độ chạy", "Đi nhanh hơn", 100, 500, function(val)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val
+    end)
+    s4:NewToggle("Fly", "Bay trên không", function(v)
+        _G.FlyEnabled = v
+        -- TODO: code fly
+    end)
 
-    -- Teleport
-    do
-        local t = Window:NewTab("🌎 Teleport")
-        local s = t:NewSection("Map / NPC")
-        s:NewButton("Sea 1", "", function() end)
-        s:NewButton("Sea 2", "", function() end)
-        s:NewButton("Sea 3", "", function() end)
-        s:NewButton("Sword Seller", "", function() end)
-        s:NewButton("Fruit Dealer", "", function() end)
-        s:NewButton("Đến đảo tùy chọn", "", function() end)
-    end
-
-    -- Awakening / Haki
-    do
-        local t = Window:NewTab("🌀 Awaken/Haki")
-        local s = t:NewSection("Tăng sức mạnh")
-        s:NewToggle("Auto bật Haki", "", function(v) _G.AutoHaki = v end)
-        s:NewButton("Auto V2/V3 Haki", "", function() end)
-        s:NewButton("Auto Awakening Skill", "", function() end)
-    end
-
-    -- Trái Ác Quỷ
-    do
-        local t = Window:NewTab("🍇 Devil Fruit")
-        local s = t:NewSection("DF / ESP")
-        s:NewToggle("ESP Trái", "", function(v) _G.ESPFruit = v end)
-        s:NewToggle("Auto nhặt Trái", "", function(v) _G.AutoPickFruit = v end)
-        s:NewButton("Auto mua Trái", "", function() end)
-    end
-
-    -- Item / Quest
-    do
-        local t = Window:NewTab("📦 Item/Quest")
-        local s = t:NewSection("Task & Item")
-        s:NewToggle("Auto nhận nhiệm vụ", "", function(v) _G.AutoQuest = v end)
-        s:NewButton("Lấy Saber", "", function() end)
-        s:NewButton("Lấy Yama", "", function() end)
-        s:NewButton("Mở cửa Yama", "", function() end)
-    end
-
-    -- Auto Raid
-    do
-        local t = Window:NewTab("🔥 Auto Raid")
-        local s = t:NewSection("Raid")
-        s:NewButton("Auto Join Raid", "", function() end)
-        s:NewButton("Auto Awakening Raid", "", function() end)
-    end
-
-    -- ESP & Misc
-    do
-        local t = Window:NewTab("👻 ESP & Misc")
-        local s = t:NewSection("Khác")
-        s:NewToggle("ESP Người Chơi", "", function(v) _G.ESPP = v end)
-        s:NewToggle("ESP Chest", "", function(v) _G.ESPChest = v end)
-        s:NewToggle("NoClip/Fly", "", function(v) _G.NoClip = v end)
-        s:NewSlider("Speed", "Tốc độ", 16, 200, function(v)
-            if game.Players.LocalPlayer.Character then
-                game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
-            end
-        end)
-    end
-end
+    -- Tab khác, Nhiệm vụ, Auto Raid... anh thêm tùy chỉnh dán vào sau đây
+end)  -- <-- Kết thúc Window:Init
