956210d426962c86524d3491ed806abbaed155f2
anhteo94 <phamvannghia20066@gmail.com>
2025-06-19 03:31:56 +0700

    Update PhamNghiaIOS_MinGaming.lua

--- anhteo94/PhamNghiaIOS_MinGaming.lua
+++ anhteo94/PhamNghiaIOS_MinGaming.lua
@@ -1,31 +1,33 @@
 -- Phạm Nghĩa IOS – Min Gaming Style Full – Sea 3
 
-local Library = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"
-))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Phạm Nghĩa IOS", "Midnight")
+Window:MakeDraggable(true)
+Window:ChangeBackground("https://raw.githubusercontent.com/anhteo94/HanhTrinhMoi/main/background_anh.png")
 
--- Thiết lập background là ảnh của anh
-Window:ChangeBackground("https://raw.githubusercontent.com/anhteo94/HanhTrinhMoi/main/AnhCuaAnh.png")
+Window:Init(function()
 
--- Tab: Tộc V4
-local t1 = Window:NewTab("Tộc V4")
-local s1 = t1:NewSection("Tộc")
-s1:NewToggle("Auto Tộc V4", "Tự động far mobs Tộc V4", function(v) _G.AutoTocV4 = v end)
+    -- Tab: Tộc V4
+    local t1 = Window:NewTab("Tộc V4")
+    local s1 = t1:NewSection("Tộc")
+    s1:NewToggle("Auto Tộc V4", "Tự động far mobs Tộc V4", function(v)
+        _G.AutoTocV4 = v
+    end)
 
--- Tab: Cửa hàng
-local t2 = Window:NewTab("Cửa hàng")
-local s2 = t2:NewSection("Mua/Bán")
-s2:NewButton("Mở Shop", "Mua item từ NPC", function()
-    -- TODO: code shop
+    -- Tab: Cửa hàng
+    local t2 = Window:NewTab("Cửa hàng")
+    local s2 = t2:NewSection("Mua/Bán")
+    s2:NewButton("Mở Shop", "Mua item từ NPC", function()
+        print("Mở shop")
+    end)
+
+    -- Tab: Dịch chuyển
+    local t3 = Window:NewTab("Dịch chuyển")
+    local s3 = t3:NewSection("TP")
+    s3:NewButton("TP Mỹ", function() end)
+    s3:NewButton("TP Jack", function() end)
+
 end)
-
--- Tab: Dịch chuyển
-local t3 = Window:NewTab("Dịch chuyển")
-local s3 = t3:NewSection("TP")
-s3:NewButton("TP Mỹ", function() end)
-s3:NewButton("TP Jack", function() end)
-
 -- Tab: Người chơi
 local t4 = Window:NewTab("Người chơi")
 local s4 = t4:NewSection("Player")
